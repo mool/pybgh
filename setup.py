6d9ad2403af733dc1d99c478a2e8e6036330890d
Pablo Gutiérrez del Castillo <pablogutierrezdelc@gmail.com>
2019-03-26 12:00:47 -0300

    add install_requires config and keywords

--- setup.py
+++ setup.py
@@ -7,13 +7,15 @@ print(setuptools.find_packages())
 
 setuptools.setup(
     name='pybgh',
-    version='0.1.0',
+    version='0.1.1',
     author='Pablo Gutierrez del Castillo',
     author_email='pablogutierrezdelc@gmail.com',
     description='A BGH Smart devices API client',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/mool/pybgh',
+    keywords='BGH',
+    install_requires=['requests'],
     packages=setuptools.find_packages(),
     license='MIT',
     classifiers=[
